7975c9513c4b8b7763273221d5fa054698fa8c7a
KingOfCactus <kingcactusgamedev@gmail.com>
2025-01-13 19:58:59 -0300

    Replaces placeholders

---
+++ index.js
@@ -0,0 +1,10 @@
+const carouselElements = document.querySelectorAll(".carousel")
+
+// carouselElements.forEach(element => {
+//     console.log("foi um")
+//     new bootstrap.Carousel(element, {
+//         interval: 10,
+//         touch: false
+//       })
+// });
+
